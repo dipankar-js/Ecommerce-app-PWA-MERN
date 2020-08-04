27836694cfe3dd5717dfdb0871111fdb39c12212
Dipankar498 <barman498@gmail.com>
2020-08-04 11:28:57 +0530

    fix:Deploy

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/orders', order);
 
 // Server static assets if in Production
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(client / build));
+	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
